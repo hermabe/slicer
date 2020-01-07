5e498e24c48dfdc0cf9cfa68de7a1304cbdca6d1
Herman Berget <32880010+hermabe@users.noreply.github.com>
2020-01-07 20:02:27 +0100

    More geometry functions

--- Slicer/geometry.cpp
+++ Slicer/geometry.cpp
@@ -1,3 +1,8 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <functional>
+
 #include "geometry.h"
 
 Vector3d intersection(const Plane& plane, const Line& line)
@@ -5,4 +10,186 @@ Vector3d intersection(const Plane& plane, const Line& line)
 	// Assumes line intersects plane at a single point
 	float distance = (plane.point - line.point).dot(plane.normal) / (line.direction.dot(plane.normal));
 	return line.point + line.direction * distance;
+}
+
+float planeCosine(const Plane & plane, const Vector3d & vertex) {
+	// Cosine of angle between vertex and plane normal
+	return plane.normal.dot(vertex - plane.point);
+};
+
+std::array<int, 3> vertexPositions(const Plane & plane, const Triangle3d & triangle) {
+	constexpr float error = 1e-6;
+
+	std::array<int, 3> result;
+	auto dest = result.begin();
+	for (auto begin = triangle.vertices.cbegin(), end = triangle.vertices.cend(); begin != end; ++begin, ++dest)
+	{
+		float cosine = planeCosine(plane, *begin);
+		if (cosine > error)
+		{
+			*dest = 1;
+		}
+		else if (cosine < -error) {
+			*dest = -1;
+		}
+		else {
+			*dest = 0;
+		}
+	}
+	return result;
+}
+
+std::array<Edge3d, 2> getIntersectingEdges(const std::array<int, 3> & positions, const Triangle3d & triangle) {
+	// Assumes triangle intersects plane
+	std::array<Edge3d, 2> edges;
+	unsigned int count = 0;
+	if (positions[0] && (positions[0] * -1 == positions[1])) {
+		edges[count++] = Edge3d{ triangle.vertices[0], triangle.vertices[1] , triangle.normal };
+	}
+	if (positions[0] && (positions[0] * -1 == positions[2])) {
+		edges[count++] = Edge3d{ triangle.vertices[2], triangle.vertices[0] , triangle.normal };
+	}
+	if (positions[1] && (positions[1] * -1 == positions[2])) {
+		edges[count++] = Edge3d{ triangle.vertices[1], triangle.vertices[2], triangle.normal };
+	}
+	assert(count == 2); // Should be 2 edges
+	return edges;
+}
+
+std::vector<Edge3d> intersection(const Plane & plane, const Triangle3d & triangle)
+{
+	// Assumes triangle intersects plane
+
+	std::array<int, 3> position = vertexPositions(plane, triangle);
+	unsigned int numberOfVerticesInPlane = std::count_if(position.cbegin(), position.cend(),
+		[&](int value) {
+			return value == 0;
+		});
+
+	std::function<unsigned int(std::array<int, 3>&)> indexOfInPlane = [](std::array<int, 3> & arr) {
+		for (unsigned int i = 0; i < 3; i++)
+		{
+			if (arr[i] == 0) {
+				return i;
+			}
+		}
+		assert(false); // At least one edge should be in plane
+	};
+
+	std::vector<Edge3d> edges;
+
+	switch (numberOfVerticesInPlane)
+	{
+	case 0: {
+		// Find two intersection points, make edge
+		std::array<Edge3d, 2> intersectingEdges = getIntersectingEdges(position, triangle);
+		Vector3d start = intersection(plane, Line(intersectingEdges[0]));
+		Vector3d end = intersection(plane, Line(intersectingEdges[1]));
+		edges.emplace_back(Edge3d{ start, end, triangle.normal });
+		break;
+	}
+	case 1: {
+		unsigned int index = indexOfInPlane(position);
+		// Check if rest of triangle intersects
+		if (position[0] + position[1] + position[2] == 0) {
+			// Other two vertices are on different side of plane, find intersection
+			Edge3d opposite = Edge3d{ triangle.vertices[(index + 1) % 3], triangle.vertices[(index + 2) % 3], triangle.normal };
+			Vector3d intersect = intersection(plane, Line(opposite));
+			edges.emplace_back(Edge3d{ triangle.vertices[index], intersect, triangle.normal });
+		}
+		break;
+	}
+	case 2: {
+		// Make edge with start, end = two vertices in plane
+		unsigned int start = indexOfInPlane(position);
+		position[start] = 2; // Do not choose same edge twice
+		unsigned int end = indexOfInPlane(position);
+		edges.emplace_back(Edge3d{ triangle.vertices[start], triangle.vertices[end], triangle.normal });
+		break;
+	}
+	case 3: {
+		// Make three edges, one for each edge in triangle
+		for (unsigned int i = 0; i < 3; i++)
+		{
+			edges.emplace_back(Edge3d{ triangle.vertices[i], triangle.vertices[(i + 1) % 3] });
+		}
+		break;
+	}
+	default:
+		throw std::logic_error("A triangle cannot have more than 3 vertices.");
+	}
+	return edges;
+}
+
+float project(const Vector3d & base, const Vector3d & vector) {
+	return base.dot(vector) / base.length();
+}
+
+Vector2d project(const Plane & plane, const Vector3d & vector)
+{
+	Vector3d yAxis = plane.yAxis();
+	Vector3d shifted = vector - plane.point;
+	return Vector2d(project(plane.xAxis, shifted), project(yAxis, shifted));
+}
+
+Edge2d project(const Plane & plane, const Edge3d & edge)
+{
+	return Edge2d{ project(plane, edge.start), project(plane, edge.end), project(plane, edge.normal) };
+}
+
+Vector3d cross(const Vector3d & lhs, const Vector3d & rhs) {
+	return Vector3d(lhs[1] * rhs[2] - lhs[2] * rhs[1], lhs[2] * rhs[0] - lhs[0] * rhs[2], lhs[0] * rhs[1] - lhs[1] * rhs[0]);
+}
+
+std::vector<Triangle3d>withVertex(const std::vector<Triangle3d> & triangles, const Vector3d & vertex)
+{
+	std::vector<Triangle3d> result;
+	std::copy_if(triangles.cbegin(), triangles.cend(), result.begin(), [&](const Triangle3d & triangle) {
+		return std::any_of(triangle.vertices.cbegin(), triangle.vertices.cend(), [&](const Vector3d & triangle_vertex) {
+			return vertex.isClose(triangle_vertex);
+			});
+		});
+	return result;
+}
+
+Triangle3d withEdge(const std::vector<Triangle3d> & triangles, const Vector3d & firstVertex, const Vector3d & secondVertex)
+{
+	return withEdge(withVertex(triangles, firstVertex), secondVertex);
+}
+
+Triangle3d withEdge(const std::vector<Triangle3d> & trianglesWithVertex, const Vector3d & secondVertex)
+{
+	return withVertex(trianglesWithVertex, secondVertex)[0];
+}
+
+float directionalAngle(const Vector2d & lhs, const Vector2d & rhs)
+{
+	float angle = std::acos(lhs.dot(rhs) / (lhs.length() * rhs.length()));
+	float cross = lhs[0] * rhs[1] - lhs[1] * rhs[0];
+	if (cross < 0)
+	{
+		angle += PI;
+	}
+	return angle;
+}
+
+bool aboveOrBelow(const Plane & plane, const Triangle3d & triangle) {
+	// Returns true if all the vertices is on the same side of the plane
+	// Returns false if triangle intersects plane
+
+	std::function<bool(const Vector3d & vertex)> above = [&](const Vector3d & vertex) { return planeCosine(plane, vertex) > 0; };
+	std::function<bool(const Vector3d & vertex)> below = [&](const Vector3d & vertex) { return planeCosine(plane, vertex) < 0; };
+
+	return std::all_of(triangle.vertices.cbegin(), triangle.vertices.cend(), above)
+		|| std::all_of(triangle.vertices.cbegin(), triangle.vertices.cend(), below);
+}
+
+std::vector<Triangle3d> intersects(const std::vector<Triangle3d> & triangles, const Plane & plane)
+{
+	std::vector<Triangle3d> result(triangles.size());
+	auto it = std::copy_if(triangles.cbegin(), triangles.cend(), result.begin(), [&](const Triangle3d & triangle) {
+		return !aboveOrBelow(plane, triangle);
+		});
+	result.resize(std::distance(result.begin(), it));
+	return result;
 }
--- Slicer/geometry.h
+++ Slicer/geometry.h
@@ -4,15 +4,18 @@
 #include <vector>
 #include <array>
 
+constexpr float PI = 3.14159265359;
+
 template<typename T, unsigned int N>
 class Vector
 {
 public:
 	template<typename... Args>
 	Vector<T, N>(Args... args) : elements{ args... } {
+		static_assert(std::is_arithmetic<T>::value, "Type must be numeric");
 		static_assert(sizeof... (Args) == N, "Wrong number of elements supplied in constructor");
 	}
-	Vector<T, N>();
+	Vector<T, N>() = default;
 	template<unsigned int M>
 	Vector<T, N>(const Vector<T, M>& rhs);
 
@@ -32,49 +35,84 @@ public:
 	T operator[](unsigned int i) const;
 
 	void normalize();
+	Vector<T, N> normalized() const;
 	T length() const;
 	T dot(const Vector<T, N>& rhs) const;
+	bool isClose(const Vector<T, N>& rhs, T tolerance = 1e-6) const;
 private:
-	T elements[N];
+	T elements[N]{};
 };
 
 typedef Vector<float, 3> Vector3d;
 typedef Vector<float, 2> Vector2d;
 
+Vector3d cross(const Vector3d& lhs, const Vector3d& rhs);
+
+template<typename T, unsigned int N>
 class Triangle
 {
 public:
-	Vector3d normal;
-	//Vector3d vertices[3];
-	std::array<Vector3d, 3> vertices;
+	Vector<T, N> normal;
+	std::array<Vector<T, N>, 3> vertices;
 };
 
-struct Plane {
-	Vector3d point, normal;
-};
+typedef Triangle<float, 3> Triangle3d;
+typedef Triangle<float, 2> Triangle2d;
 
-struct Line {
-	Vector3d point, direction;
-};
-
+template<typename T, unsigned int N>
 struct Edge {
-	Vector2d start, end, normal;
+	Vector<T, N> start, end, normal;
 };
 
-struct Polygon {
+typedef Edge<float, 3> Edge3d;
+typedef Edge<float, 2> Edge2d;
+
+class Plane {
+public:
+	Vector3d point, normal, xAxis;
+
+	Plane(const Vector3d& point, const Vector3d& normal, const Vector3d& xAxis) : point(point), normal(normal.normalized()), xAxis(xAxis.normalized()) {};
+	Vector3d yAxis() const {
+		return cross(normal, xAxis).normalized();
+	}
+};
+
+class Line {
+public:
+	Vector3d point, direction;
+
+	Line(const Edge3d& edge) : point(edge.start), direction(edge.end - edge.start) {};
+	Line(const Vector3d& point, const Vector3d& direction) : point(point), direction(direction) {};
+};
+
+struct SimplePolygon {
+	// Represents a simply connected polygon
 	std::vector<Vector2d> vertices;
 };
 
-Vector3d intersection(const Plane& plane, const Line& line);
+struct Polygon {
+	// Represents a complex polygon.
+	// Can have zero or more "holes" which are SimplePolygons
+	SimplePolygon exterior;
+	std::vector<SimplePolygon> interior;
+};
 
-template<typename T, unsigned int N>
-inline Vector<T, N>::Vector()
-{
-	for (unsigned int i = 0; i < N; i++)
-	{
-		elements[i] = 0;
-	}
-}
+struct MultiPolygon {
+	std::vector<Polygon> polygons;
+};
+
+std::vector<Triangle3d> intersects(const std::vector<Triangle3d>& triangles, const Plane& plane);
+Vector3d intersection(const Plane& plane, const Line& line);
+std::vector<Edge3d> intersection(const Plane& plane, const Triangle3d& triangle);
+
+Vector2d project(const Plane& plane, const Vector3d& vector);
+Edge2d project(const Plane& plane, const Edge3d& edge);
+
+std::vector<Triangle3d> withVertex(const std::vector<Triangle3d>& triangles, const Vector3d& vertex);
+Triangle3d withEdge(const std::vector<Triangle3d>& triangles, const Vector3d& firstVertex, const Vector3d& secondVertex);
+Triangle3d withEdge(const std::vector<Triangle3d>& withVertex, const Vector3d& secondVertex);
+
+float directionalAngle(const Vector2d& lhs, const Vector2d& rhs);
 
 template<unsigned int a, unsigned int b>
 struct min {
@@ -177,6 +215,12 @@ inline void Vector<T, N>::normalize()
 }
 
 template<typename T, unsigned int N>
+inline Vector<T, N> Vector<T, N>::normalized() const
+{
+	return *this / length();
+}
+
+template<typename T, unsigned int N>
 inline T Vector<T, N>::length() const
 {
 	return std::sqrtf(this->dot(*this));
@@ -192,6 +236,12 @@ inline T Vector<T, N>::dot(const Vector<T, N> & rhs) const
 	}
 
 	return result;
+}
+
+template<typename T, unsigned int N>
+inline bool Vector<T, N>::isClose(const Vector<T, N> & rhs, T tolerance) const
+{
+	return (*this - rhs).length() < tolerance;
 }
 
 template<typename T, unsigned int N>
